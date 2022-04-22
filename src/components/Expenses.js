86b2fb3478e9e02eb8c91839906142179c2899d7
Juan-45 <quijuan12@gmail.com>
2022-04-22 12:21:14 -0300

    Add styles importation

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,13 +1,19 @@
 import ExpenseItem from "./expenses/ExpenseItem";
+import "./expenses/Expenses.css";
 
-const Expenses = (props) =>
-  props.expenseArr.map((item) => (
-    <ExpenseItem
-      title={item.title}
-      amount={item.amount}
-      date={item.date}
-      key={item.id}
-    />
-  ));
+const Expenses = (props) => {
+  return (
+    <div className="expenses">
+      {props.expenseArr.map((item) => (
+        <ExpenseItem
+          title={item.title}
+          amount={item.amount}
+          date={item.date}
+          key={item.id}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default Expenses;
